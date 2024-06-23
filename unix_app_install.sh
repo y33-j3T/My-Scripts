b2b167f813c1556ef751bc80426240ca8d5288c7
y33-j3T <yeejet.tech@gmail.com>
2024-06-23 22:07:21 +0800

    add unix app installation commands

---
+++ unix_app_install.sh
@@ -0,0 +1,5 @@
+sudo apt remove <package-name>  # remove app only
+sudo apt purge <package-name>   # remove app & config
+sudo apt clean       # clear package cache
+sudo apt autoremove  # remove unrequired dependencies
+sudo apt -f install  # fix broken install
